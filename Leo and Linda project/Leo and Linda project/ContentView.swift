60eea057cfa1a5d98b3c1ee5c7000a168790399e
Linda Woo <59804818+lindakwoo@users.noreply.github.com>
2024-01-20 14:47:42 -0800

    change input fields

--- Leo and Linda project/Leo and Linda project/ContentView.swift
+++ Leo and Linda project/Leo and Linda project/ContentView.swift
@@ -7,166 +7,6 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State var numPlayers: String = ""
-    @State var numCourts: String = ""
-//    @State var numGamesPlayedPerPlayer: String = ""
-    @State var numPlayersText: String = ""
-    @State var courtsNumText: String = ""
-//    @State var gamesPerPlayerText: String = ""
-    @State var courts: String = "courts"
-    @State var schedule = [[Game]]() 
-    @State private var playerNames: [String] = []
-    
-    func genSchedule() {
-         guard let playersNum = Int(numPlayers),
-              let courtsNum = Int(numCourts) else {
-            self.numPlayersText = "Invalid input. Please enter valid numbers."
-            return
-        }
-
-        schedule = generateSchedule(numPlayers: playersNum, numCourts: courtsNum, playerNames: playerNames)
-    }
-
-     // Check if all player names are filled out
-    func allPlayerNamesFilled() -> Bool {
-        return !playerNames.contains { $0.isEmpty }
-    }
-
-    func isPositiveInteger(_ input: String) -> Bool {
-        if let number = Int(input), number > 0 {
-            return true
-        }
-        return false
-    }
-
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                VStack(alignment: .center) {
-                    Text("Pickleball Shuffle")
-                        .font(.title)
-                        .multilineTextAlignment(.center)
-                        .padding()
-                    Image("cindypickle 1")
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .padding(.bottom, 20)
-
-                    Group {
-                        InputFieldView(label: "Number of players:", text: $numPlayers, placeholder: "Enter number of players")
-                            .onChange(of: numPlayers) { newValue in
-                                guard Int(numPlayers) != nil else {
-                                    self.numPlayersText = "Please enter a valid number for courts."
-                                    return
-                                }
-                                if let playerCount = Int(numPlayers), playerCount > 3 {
-                                    playerNames = Array(repeating: "", count: playerCount)
-                                    numPlayersText = !allPlayerNamesFilled() ? "Enter the names of your \(playerCount) players below": ""
-                                } else {
-                                     playerNames = Array(repeating: "", count: 0)
-                                    numPlayersText = "You must have at least 4 players"
-                                    
-                                }
-                            }
-                        Text(numPlayersText)
-                            .font(.callout)
-                            .foregroundColor(.red)
-                        
-                        ForEach(0..<playerNames.count, id: \.self) { index in
-                            TextField("Player \(index + 1)", text: $playerNames[index])
-                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                .onChange(of:playerNames[index]){newValue in
-                                numPlayersText = !allPlayerNamesFilled() ? "Enter the names of your \(numPlayers) players below": "" }
-                        }
-                        
-                        InputFieldView(label: "Number of courts:", text: $numCourts, placeholder: "Enter number of courts")
-                            .onChange(of: numCourts) { newValue in
-                                guard let playersNum = Int(numPlayers), let courtsNum = Int(newValue) else {
-                                    self.courtsNumText = "Please enter a valid number for courts."
-                                    return
-                                }
-                                let maxCourtsAllowed = playersNum/4
-                                self.courtsNumText = courtsNum > maxCourtsAllowed ? "Input exceeds limit: Maximum \(maxCourtsAllowed) courts." : ""
-                            }
-                        Text(courtsNumText)
-                            .font(.callout)
-                            .foregroundColor(.red)
-
-//                        InputFieldView(label: "Games per player:", text: $numGamesPlayedPerPlayer, placeholder: "Enter games per player")
-//                            .onChange(of: numGamesPlayedPerPlayer) { newValue in
-//                                guard let gamesNum = Int(newValue), let playersNum = Int(numPlayers) else {
-//                                    self.gamesPerPlayerText = "Please enter a valid number for games."
-//                                    return
-//                                }
-//                                let maxGamesPerPlayerAllowed = playersNum-1
-//                                self.gamesPerPlayerText = gamesNum > maxGamesPerPlayerAllowed ? "Input exceeds limit: Maximum \(maxGamesPerPlayerAllowed) games per player before games repeat. After \(maxGamesPerPlayerAllowed) games, start over at the beginning of the schedule at round 1.": ""
-//                            }
-//                        Text(gamesPerPlayerText)
-//                            .font(.callout)
-//                            .foregroundColor(.red)
-                    }
-
-                    Spacer(minLength: 20)
-                    
-                    NavigationLink(
-                        destination: scheduleDestinationView,
-                        label: {
-                            Text("Generate Schedule")
-                                .foregroundColor(.white)
-                                .frame(minWidth: 200, minHeight: 50)
-                                .background(Color.green)
-                                .cornerRadius(10)
-                        }
-                    )
-                    .padding()
-                    .disabled(!(allPlayerNamesFilled() && isPositiveInteger(numCourts) && courtsNumText.isEmpty))
-                    .opacity((allPlayerNamesFilled() && isPositiveInteger(numCourts) && courtsNumText.isEmpty) ? 1.0 : 0.5)
-                }
-                .padding()
-    
-            }
-        }
-    }
-
-    // Destination view for the NavigationLink
-    private var scheduleDestinationView: some View {
-        if allPlayerNamesFilled() && isPositiveInteger(numCourts){
-           // Set the text before navigating
-            return AnyView(ScheduleView(schedule: schedule, numCourts: numCourts).onAppear(perform: genSchedule))
-        } else {
-            return AnyView(
-                VStack {
-                    Text("All player names must be filled out.")
-                        .foregroundColor(.red)
-                        .padding()
-                    Spacer()
-                }
-            )
-        }
-    }
-}
-
-struct InputFieldView: View {
-    var label: String
-    @Binding var text: String
-    var placeholder: String
-
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(label)
-                .font(.headline)
-            TextField(placeholder, text: $text)
-                .foregroundColor(.white)
-                .padding(10)
-                .frame(height: 44)
-                .background(Color.green)
-                .keyboardType(.decimalPad)
-                .cornerRadius(5)
-        }
-    }
-}
-
 extension Array {
     func chunked(into size: Int) -> [[Element]] {
         return stride(from: 0, to: count, by: size).map {
@@ -226,6 +66,175 @@ struct ScheduleView: View {
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @State var numPlayers: String = ""
+    @State var numCourts: String = ""
+    @State var numPlayersText: String = ""
+    @State var courtsNumText: String = ""
+    @State var courts: String = "courts"
+    @State var schedule = [[Game]]()
+    @State private var playerName: String = ""
+    @State private var players: [String] = []
+    @State var enterPlayersText: String = "Enter at least 4 players"
+    
+    func genSchedule() {
+        guard let playersNum = Int(numPlayers),
+              let courtsNum = Int(numCourts) else {
+            self.numPlayersText = "Invalid input. Please enter valid numbers."
+            return
+        }
+        
+        schedule = generateSchedule(numPlayers: playersNum, numCourts: courtsNum, playerNames: players)
+    }
+    
+    // Check if all player names are filled out
+    func allPlayerNamesFilled() -> Bool {
+        return !players.contains { $0.isEmpty }
+    }
+    
+    func isPositiveInteger(_ input: String) -> Bool {
+        if let number = Int(input), number > 0 {
+            return true
+        }
+        return false
+    }
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .center) {
+                    Text("Pickleball Shuffle")
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    Image("cindypickle 1")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .padding(.bottom, 20)
+                    
+                    Group {
+                        // Single input field for player name
+                        HStack{
+                            TextField("Player Name", text: $playerName)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                            
+                            // Button to add player
+                            Button(action: {
+                                if !playerName.isEmpty {
+                                    players.append(playerName)
+                                    playerName = ""
+                                    numPlayers = String(players.count)
+                                    enterPlayersText = players.count > 3 ? "": "Enter at least 4 players"
+                                }
+                            }) {
+                                Image(systemName: "plus.circle.fill")
+                                    .foregroundColor(.green)
+                                    .font(.title)
+                            }
+                        }
+                        // List of players with delete button
+                        ForEach(players, id: \.self) { player in
+                            HStack {
+                                Text(player)
+                                    .foregroundColor(.gray)
+                                Spacer()
+                                Button(action: {
+                                    if let index = players.firstIndex(of: player) {
+                                        players.remove(at: index)
+                                        numPlayers = String(players.count)
+                                        enterPlayersText = players.count > 3 ? "": "Enter at least 4 players"
+                                    }
+                                }) {
+                                    Image(systemName: "x.circle.fill")
+                                        .foregroundColor(.red)
+                                }
+                            }
+                            .padding()
+                            .background(RoundedRectangle(cornerRadius: 10)
+                                                           .fill(Color.white)
+                                                           .border(Color.gray))
+                        }
+                    }
+                    
+                    InputFieldView(label: "Number of courts:", text: $numCourts, placeholder: "Enter number of courts")
+                        .onChange(of: numCourts) { newValue in
+                            guard let playersNum = Int(numPlayers), let courtsNum = Int(newValue) else {
+                                self.courtsNumText = "Please enter a valid number for courts."
+                                return
+                            }
+                            let maxCourtsAllowed = playersNum/4
+                            self.courtsNumText = courtsNum > maxCourtsAllowed ? "Input exceeds limit: Maximum \(maxCourtsAllowed) courts." : ""
+                        }
+                    Text(courtsNumText)
+                        .font(.callout)
+                        .foregroundColor(.red)
+                    
+                    Spacer(minLength: 20)
+                    Text(enterPlayersText)
+                    
+                    NavigationLink(
+                        destination: scheduleDestinationView,
+                        label: {
+                            Text("Generate Schedule")
+                                .foregroundColor(.white)
+                                .frame(minWidth: 200, minHeight: 50)
+                                .background(Color.green)
+                                .cornerRadius(10)
+                        }
+                    )
+                    .padding()
+                    .disabled(!(Int(numPlayers) ?? 2 > 3 && isPositiveInteger(numCourts) && courtsNumText.isEmpty))
+                    .opacity((Int(numPlayers) ?? 2 > 3  && isPositiveInteger(numCourts) && courtsNumText.isEmpty) ? 1.0 : 0.5)
+                }
+                .padding()
+                
+            }
+        }
+    }
+    
+    // Destination view for the NavigationLink
+    private var scheduleDestinationView: some View {
+        if isPositiveInteger(numCourts){
+            // Set the text before navigating
+            return AnyView(ScheduleView(schedule: schedule, numCourts: numCourts).onAppear(perform: genSchedule))
+        } else {
+            return AnyView(
+                VStack {
+                    Text("All player names must be filled out.")
+                        .foregroundColor(.red)
+                        .padding()
+                    Spacer()
+                }
+            )
+        }
+    }
 }
+
+
+struct InputFieldView: View {
+    var label: String
+    @Binding var text: String
+    var placeholder: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(label)
+                .font(.headline)
+            TextField(placeholder, text: $text)
+                .foregroundColor(.white)
+                .padding(10)
+                .frame(height: 44)
+                .background(Color.green)
+                .keyboardType(.decimalPad)
+                .cornerRadius(5)
+        }
+    }
+}
+
+
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
